8cfd78fe5d72eb26fc0a254c26e062140860a957
liuju <liuju@OLIVER-WORK>
2018-03-05 14:42:24 +0800

    开始产品的详情页面

---
+++ eContract.Web/Areas/LUBR/Controllers/ProductDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eContract.Web.Areas.LUBR.Controllers
+{
+    public class ProductDetailsController : BaseController
+    {
+        // GET: LUBR/ProductDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
